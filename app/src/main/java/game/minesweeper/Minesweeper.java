9c7af9b98a923f8932a6ae5fa87ad99e0c5487c5
matteocrosariol <matteo.crosariol@gmail.com>
2026-02-19 16:20:08 +0100

    git test

--- app/src/main/java/game/minesweeper/Minesweeper.java
+++ app/src/main/java/game/minesweeper/Minesweeper.java
@@ -3,7 +3,7 @@ package game.minesweeper;
 public class Minesweeper {
 
     public String getGreeting() {
-        return "Welcome to Minesweeper!!";
+        return "Welcome to Minesweeper!! This is the entry point";
     }
 
     public static void main(String[] args) {
