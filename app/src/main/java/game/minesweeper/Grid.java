40c1f733ef579bb2dc531c49c8696f3c19ef094c
matteocrosariol <matteo.crosariol@gmail.com>
2026-03-08 16:51:10 +0100

    feat: added getNeighborCoordinate method in Grid for return the neighbor's coordinates

--- app/src/main/java/game/minesweeper/Grid.java
+++ app/src/main/java/game/minesweeper/Grid.java
@@ -29,4 +29,27 @@ public class Grid extends AbstractGrid2D {
         return neighbors;
     }
 
+    public List<Coordinate> getNeighborCoordinates(Coordinate coordinate) {
+
+        int row = coordinate.row();
+        int column = coordinate.column();
+
+        List<Coordinate> neighbors = new ArrayList<>();
+
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int dc = -1; dc <= 1; dc++) {
+
+                if (dr == 0 && dc == 0) continue;
+
+                Coordinate neighbor = new Coordinate(row + dr, column + dc);
+
+                if (cells.containsKey(neighbor)) {
+                    neighbors.add(neighbor);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
 }
