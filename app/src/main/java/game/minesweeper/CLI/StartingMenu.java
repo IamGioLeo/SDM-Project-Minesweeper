685134a773625f4ff6cf6958211ffda49c90e7cb
Leo <gioleo999@gmail.com>
2026-05-10 10:47:50 +0200

    feat: now StartingMenu starts the game, now starting menu has options exit, help, and starting gui

--- app/src/main/java/game/minesweeper/CLI/StartingMenu.java
+++ app/src/main/java/game/minesweeper/CLI/StartingMenu.java
@@ -1,5 +1,8 @@
 package game.minesweeper.CLI;
 
+import game.minesweeper.GUI.StartingMenuGUI;
+import game.minesweeper.engine.Game;
+import game.minesweeper.engine.GameController;
 import game.minesweeper.engine.GridInitializer;
 import game.minesweeper.grid.Coordinate;
 import game.minesweeper.grid.GridOfSquares;
@@ -8,17 +11,20 @@ import java.util.Scanner;
 
 public class StartingMenu {
 
-    public static GridOfSquares startMenu() {
+    private static final Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+    public static void startMenu() {
 
         int columns = 9;
         int rows = 9;
         int mines = 10;
 
+        System.out.println("Chose the defaults game difficulties: easy (e), medium (m), hard (h)");
+        System.out.println("Or personalize your game: personalize (p)");
+        System.out.println("To play on the Graphical User Interface: gui (g)");
+
+        menuLoop:
         while (true) {
-            System.out.println("Chose the game difficulty: easy (e), medium (m), hard (h)");
-            System.out.println("Or personalized setting: personalize (p)");
 
             String option = scanner.next().toLowerCase();
 
@@ -37,6 +43,7 @@ public class StartingMenu {
                     rows = 30;
                     columns = 16;
                     mines = 99;
+                    break;
 
                 case "personalized", "p":
                     System.out.println("How many Columns?");
@@ -45,24 +52,64 @@ public class StartingMenu {
                     rows = scanner.nextInt();
                     System.out.println("How many mines?");
                     mines = scanner.nextInt();
-                    if (columns*rows < mines) {
+                    if (columns * rows < mines) {
                         System.out.println("There are too many mines!");
                         continue;
                     }
                     break;
 
+                case "gui", "g":
+                    new StartingMenuGUI().startMenu();
+                    return;
+
+                case "help":
+                    System.out.println("Chose the defaults game difficulties: easy (e), medium (m), hard (h)");
+                    System.out.println("Or access personalized setting: personalize (p)");
+                    System.out.println("To play on the Graphical User Interface: gui (g)");
+                    System.out.println("To exit: exit");
+                    continue;
+
+                case "exit":
+                    break menuLoop;
+
                 default:
                     System.out.println("Invalid option!");
                     continue;
             }
-            break;
+
+            while (true) {
+                startGame(rows, columns, mines);
+
+                System.out.println("Do you want to start a new game? (y/n)");
+                String newGame = scanner.next().toLowerCase();
+
+                switch (newGame) {
+                    case "y", "yes":
+                        continue;
+                    case "menu":
+                        System.out.println("Chose the defaults game difficulties: easy (e), medium (m), hard (h)");
+                        System.out.println("Or personalize your game: personalize (p)");
+                        System.out.println("To play on the Graphical User Interface: gui (g)");
+
+                        continue menuLoop;
+                    default:
+                        return;
+                }
+            }
         }
+    }
+
+    public static void startGame(int rows, int columns, int mines) {
 
         GridOfSquares grid = new GridOfSquares(rows, columns);
         GridInitializer<Coordinate> gridInitializer = new GridInitializer<>(grid);
         gridInitializer.initialize(mines);
 
-        return grid;
+        Game<Coordinate> game = new Game<>(grid);
+        GameController controller = new GameController(game);
+
+        ConsoleUI ui = new ConsoleUI();
+        ui.start(controller, grid);
 
     }
 
--- app/src/main/java/game/minesweeper/Minesweeper.java
+++ app/src/main/java/game/minesweeper/Minesweeper.java
@@ -1,13 +1,5 @@
 package game.minesweeper;
 
-import game.minesweeper.CLI.ConsoleUI;
-import game.minesweeper.engine.Game;
-import game.minesweeper.engine.GameController;
-import game.minesweeper.grid.Coordinate;
-import game.minesweeper.grid.GridOfSquares;
-
-import java.util.Scanner;
-
 import static game.minesweeper.CLI.StartingMenu.startMenu;
 
 public class Minesweeper {
@@ -18,22 +10,6 @@ public class Minesweeper {
 
         System.out.println("Welcome to Minesweeper!");
 
-        Scanner scanner = new Scanner(System.in);
-        String newGame;
-
-        do {
-            GridOfSquares grid = startMenu();
-
-            Game<Coordinate> game = new Game<>(grid);
-            GameController controller = new GameController(game);
-
-            ConsoleUI ui = new ConsoleUI();
-            ui.start(controller, grid);
-
-            System.out.println("Do you want to start a new game?");
-            newGame = scanner.next().toLowerCase();
-
-        } while (newGame.equals("yes") || newGame.equals("y"));
+        startMenu();
     }
-
 }
